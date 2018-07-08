4ff1d7a0155623131526daac430bc349864e65d0
Darie95 <dariannochka95@mail.ru>
2018-07-08 11:59:24 +0200

    Adding testing for new homework

--- HomeworkDjango/tests.py
+++ HomeworkDjango/tests.py
@@ -2,6 +2,8 @@ from time import sleep
 
 from django.test import TestCase
 from selenium import webdriver
+import random
+import string
 
 
 class ItemTests(TestCase):
@@ -20,3 +22,15 @@ class ItemTests(TestCase):
         rows = self.driver.find_elements_by_tag_name('tr')
         result_id = [int(row.get_attribute('data-item-id')) for row in rows[1:]]
         self.assertEqual([1, 6], result_id)
+
+    def test_item_create(self):
+        self.driver.get('http://localhost:8000/item_create/')
+        new_name = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
+        self.driver.find_element_by_id('id_name').send_keys(new_name)
+        self.driver.find_element_by_id('id_price').send_keys('100')
+        self.driver.find_element_by_xpath("//select/option[@value='1']").click()
+        self.driver.find_element_by_tag_name('button').click()
+        sleep(10)
+        self.assertTrue(new_name in self.driver.page_source)
+
+
