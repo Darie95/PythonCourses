52a38981007b21d461b70806cb88b2dc11ebcc9a
Darie95 <dariannochka95@mail.ru>
2018-07-08 13:19:05 +0200

    Checking odd spaces

--- HomeworkDjango/forms.py
+++ HomeworkDjango/forms.py
@@ -14,7 +14,8 @@ class SimpleForm(forms.Form):
     price = forms.IntegerField(label='Цена', min_value=5, max_value=3000)
     is_sold = forms.BooleanField(label='Продано')
     creation_date = forms.DateField(
-        label='Дата создания', widget=forms.TextInput(attrs={'class': 'datepicker'}))
+        label='Дата создания',
+        widget=forms.TextInput(attrs={'class': 'datepicker'}))
 
 
 class PictureWidget(forms.widgets.Widget):
@@ -27,19 +28,20 @@ class ItemCreateForm(forms.ModelForm):
     class Meta:
         image = ImageField(widget=PictureWidget)
         model = Item
-        fields = ('name', 'description','price', 'is_sold','department',
-                'creation_date', 'image')
+        fields = ('name', 'description', 'price', 'is_sold', 'department',
+                  'creation_date', 'image')
 
 
 class SearchForm(forms.Form):
     min_price = forms.IntegerField(label='Минимальная цена', min_value=0,
-                                   max_value=10**12, required = False)
+                                   max_value=10 ** 12, required=False)
     max_price = forms.IntegerField(label='Максимальная цена', min_value=0,
-                                   max_value=10**12, required = False)
-    part_name = forms.CharField(label='Часть названия', max_length=10, required = False)
+                                   max_value=10 ** 12, required=False)
+    part_name = forms.CharField(label='Часть названия', max_length=10,
+                                required=False)
     is_sold = forms.BooleanField(label='Продано', required=False)
     shop = forms.ModelChoiceField(label='Магазин',
-            queryset= Shop.objects.all(),required = False )
+                                  queryset=Shop.objects.all(), required=False)
 
     def clean_part_name(self):
         if ' ' in self.cleaned_data['part_name']:
